da4bfb36884bcb018e3af011517e78917b4b3475
Ian Brandt <ian@ianbrandt.com>
2023-10-15 15:32:40 -0700

    Added test case for https://github.com/spring-projects/spring-framework/issues/31439.

---
+++ subprojects/app/src/it/kotlin/org/sdkotlin/springdemo/SpringGetBeanNestedGenericIT.kt
@@ -0,0 +1,82 @@
+package org.sdkotlin.springdemo
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatNoException
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.BeanFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.getBean
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.core.ResolvableType
+
+/**
+ * Test case for
+ * [spring-projects/spring-framework#31439](https://github.com/spring-projects/spring-framework/issues/31439).
+ *
+ * Passes with:
+ *
+ * ```kotlin
+ * inline fun <reified T : Any> BeanFactory.getBean(): T =
+ * 	getBeanProvider<T>(
+ * 		ResolvableType.forType(
+ * 			(object : ParameterizedTypeReference<T>() {}).type
+ * 		)
+ * 	).`object`
+ * ```
+ */
+@SpringBootTest
+internal class SpringGetBeanNestedGenericIT {
+
+	companion object {
+
+		val LIST_OF_STRING: List<String> =
+			listOf("Testing")
+
+		val LIST_OF_LIST_OF_STRING: List<List<String>> =
+			listOf(listOf("Testing"))
+	}
+
+	@Configuration
+	class TestConfig {
+
+		@Bean
+		fun listOfString(): List<String> =
+			LIST_OF_STRING
+
+		@Bean
+		fun listOfListOfString(): List<List<String>> =
+			LIST_OF_LIST_OF_STRING
+	}
+
+	@Test
+	@Disabled
+	fun `test getBean with nested generic`(
+		applicationContext: ApplicationContext
+	) {
+		assertThatNoException().isThrownBy {
+
+			applicationContext.getBean<List<String>>()
+		}
+	}
+
+	@Test
+	fun `test listOfString is injected`(
+		@Autowired
+		listOfString: List<String>
+	) {
+		assertThat(listOfString).isEqualTo(LIST_OF_STRING)
+	}
+
+	@Test
+	fun `test listOfListOfString is injected`(
+		@Autowired
+		listOfListOfString: List<List<String>>
+	) {
+		assertThat(listOfListOfString).isEqualTo(LIST_OF_LIST_OF_STRING)
+	}
+}
