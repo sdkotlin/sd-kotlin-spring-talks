b75392cd9be965d4661961e030afc4f2394190ea
Ian Brandt <ian@ianbrandt.com>
2024-03-24 11:28:23 -0700

    Added workaround for https://github.com/gradle/gradle/issues/21364.

--- build-logic/src/main/kotlin/org.sdkotlin.buildlogic.kotlin-project.gradle.kts
+++ build-logic/src/main/kotlin/org.sdkotlin.buildlogic.kotlin-project.gradle.kts
@@ -32,4 +32,13 @@ tasks {
 			)
 		}
 	}
+
+	withType<JavaExec>().configureEach {
+
+		if (name.endsWith("main()")) {
+
+			// https://github.com/gradle/gradle/issues/21364
+			notCompatibleWithConfigurationCache("JavaExec created by IntelliJ")
+		}
+	}
 }
