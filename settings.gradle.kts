0fbe273b844da2b23bcb748daf0ec11b5078d620
Ian Brandt <ian@ianbrandt.com>
2024-03-24 11:53:31 -0700

    Dropped Gradle toolchains and Foojay Resolver Convention configuration as it wasn't really needed for this project (see also https://jakewharton.com/gradle-toolchains-are-rarely-a-good-idea/).

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,10 +6,6 @@ pluginManagement {
 	includeBuild("build-logic")
 }
 
-plugins {
-	id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
-}
-
 dependencyResolutionManagement {
 	@Suppress("UnstableApiUsage")
 	repositories {
