3e1bb778b3d68d9082159b4b20e4ee50cc14b9df
Ian Brandt <ian@ianbrandt.com>
2024-04-25 09:38:37 -0700

    Added `printCustomClasspath` ad-hoc task to print the "custom" classpath, i.e. the `ArtifactView` of the `runtimeClasspath` filtered by the custom resource attributes (not working, the entire `runtimeClasspath` is still being printed).

--- subprojects/app/build.gradle.kts
+++ subprojects/app/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.sdkotlin.buildlogic.attributes.CustomResources.CUSTOM_RESOURCE
+import org.sdkotlin.buildlogic.attributes.ResourceAttributes.applyResourceAttributes
+
 plugins {
 	application
 	alias(libs.plugins.springboot.plugin)
+	id("org.sdkotlin.buildlogic.custom-resources-consumer")
 	id("org.sdkotlin.buildlogic.spring-project")
 	id("org.sdkotlin.buildlogic.test.integration-test-suite")
 }
@@ -34,4 +38,30 @@ dependencies {
 
 application {
 	mainClass = "org.sdkotlin.springdemo.SpringBootAppKt"
+}
+
+tasks {
+
+	register("printCustomClasspath") {
+
+		val fileCollection: FileCollection =
+			configurations.runtimeClasspath.get().incoming.artifactView {
+
+				lenient(false)
+
+				@Suppress("UnstableApiUsage")
+				withVariantReselection()
+
+				attributes {
+					applyResourceAttributes(objects, CUSTOM_RESOURCE)
+				}
+			}.files
+
+		doLast {
+
+			val customResourcesAsPath = fileCollection.asPath
+
+			println("customResourcesAsPath: $customResourcesAsPath")
+		}
+	}
 }
