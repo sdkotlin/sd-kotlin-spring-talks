ce2b0dabb944082009254b35e5e1d6da3d59eca5
Ian Brandt <ian@ianbrandt.com>
2024-11-03 15:34:29 -0800

    Code cleanup.

--- build-logic/src/main/kotlin/org/sdkotlin/buildlogic/plugins/resources/ResourceConfigurationsPlugin.kt
+++ build-logic/src/main/kotlin/org/sdkotlin/buildlogic/plugins/resources/ResourceConfigurationsPlugin.kt
@@ -17,50 +17,52 @@ class ResourceConfigurationsPlugin : Plugin<Project> {
 		project.pluginManager.apply(JavaBasePlugin::class.java)
 
 		val extension = project.extensions
-			.create<ResourceConfigurationsExtension>("resourceConfigs")
+				.create<ResourceConfigurationsExtension>("resourceConfigs")
 
-		extension.resourceConfigurations.get()
-			.forEach { resourceConfiguration ->
+		extension.resourceConfigurations.get().forEach { resourceConfiguration ->
 
-				// Create a variant-aware consumable configuration for this resource
-				// configuration's artifacts.
-				val consumableConfigurationName =
-					"${resourceConfiguration}Elements"
-				@Suppress("UnstableApiUsage")
-				project.configurations.consumable(consumableConfigurationName) {
-					attributes {
-						applyLibraryElementsAttributes(project.objects,
-							resourceConfiguration)
-					}
-				}
-
-				// Add a `DependencyHandler` extension for declaring a dependency on
-				// the resource configuration's artifact variant.
-				val dependencyHandlerExtensionName =
-					"${resourceConfiguration}Resources"
-				project.dependencies.extensions.add(
-					DependencyCreationExtension::class.java,
-					dependencyHandlerExtensionName,
-					LibraryElementsAttributeDependencyCreationExtension(
-						dependencyHandler = project.dependencies,
-						objects = project.objects,
-						libraryElementsAttributeValue = resourceConfiguration
-					)
-				)
-
-				// Any files in "src/main/<resourceConfiguration>/" are resources for this
-				// configuration.
-				val resourcesDirectory: Directory =
-					project.layout.projectDirectory.dir("src/main/$resourceConfiguration")
-
-				// No build step is necessary, so directly add the directory as an
-				// artifact to the variant-aware consumable configuration (if it exists).
-				if (resourcesDirectory.asFile.exists()) {
-					project.artifacts.add(consumableConfigurationName,
-						resourcesDirectory) {
-						type = JVM_RESOURCES_DIRECTORY
-					}
+			// Create a variant-aware consumable configuration for this resource
+			// configuration's artifacts.
+			val consumableConfigurationName = "${resourceConfiguration}Elements"
+			@Suppress("UnstableApiUsage")
+			project.configurations.consumable(consumableConfigurationName) {
+				attributes {
+					applyLibraryElementsAttributes(project.objects,
+						resourceConfiguration)
 				}
 			}
+
+			// Add a `DependencyHandler` extension for declaring a dependency on
+			// the resource configuration's artifact variant.
+			val dependencyHandlerExtensionName = "${resourceConfiguration}Resources"
+			project.dependencies.extensions.add(
+				DependencyCreationExtension::class.java,
+				dependencyHandlerExtensionName,
+				LibraryElementsAttributeDependencyCreationExtension(
+					dependencyHandler = project.dependencies,
+					objects = project.objects,
+					libraryElementsAttributeValue =
+						"$resourceConfiguration resources"
+				)
+			)
+
+			// Any files in "src/main/<resourceConfiguration>/" are resources
+			// for this configuration.
+			val resourcesDirectory: Directory =
+				project.layout.projectDirectory
+						.dir("src/main/$resourceConfiguration")
+
+			// No build step is necessary, so directly add the directory as an
+			// artifact to the variant-aware consumable configuration (if it
+			// exists).
+			if (resourcesDirectory.asFile.exists()) {
+				project.artifacts.add(
+					consumableConfigurationName,
+					resourcesDirectory
+				) {
+					type = JVM_RESOURCES_DIRECTORY
+				}
+			}
+		}
 	}
 }
