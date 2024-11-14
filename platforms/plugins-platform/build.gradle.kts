5db88c13b2e923ca50f3f4a1ff92879423640f3a
Ian Brandt <ian@ianbrandt.com>
2024-11-14 07:52:03 -0800

    Revert "Dropped Spring Boot Gradle Plugin (not needed given the Application plugin, and it currently seems to not be compatible with `org.gradle.unsafe.isolated-projects=true`)."
    This reverts commit e1bcb001f2b1087f1d9079403fea942137e32639.

--- platforms/plugins-platform/build.gradle.kts
+++ platforms/plugins-platform/build.gradle.kts
@@ -7,5 +7,6 @@ dependencies {
 		api(libs.jvm.dependency.conflict.resolution.gradle.plugin.dependency)
 		api(libs.kotlin.gradle.plugin.dependency)
 		api(libs.kotlin.spring.gradle.plugin.dependency)
+		api(libs.springboot.gradle.plugin.dependency)
 	}
 }
