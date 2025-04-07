4b3206dc90fb26d04aeac211238ef8ba2f3cadb5
Ian Brandt <ian@ianbrandt.com>
2025-04-07 13:46:56 -0700

    Corrected KDoc.

--- build-logic/src/main/kotlin/org/sdkotlin/buildlogic/attributes/CurrentOsAttributeDisambiguationRule.kt
+++ build-logic/src/main/kotlin/org/sdkotlin/buildlogic/attributes/CurrentOsAttributeDisambiguationRule.kt
@@ -2,23 +2,16 @@ package org.sdkotlin.buildlogic.attributes
 
 import org.gradle.api.attributes.AttributeDisambiguationRule
 import org.gradle.api.attributes.MultipleCandidatesDetails
-import org.gradle.api.model.ObjectFactory
 import org.gradle.nativeplatform.OperatingSystemFamily
 import javax.inject.Inject
 
 /**
  * A rule to disambiguate between multiple candidates of the
- * [OperatingSystemFamily] attribute in Gradle. It ensures that the attribute
- * value most closely matching the current operating system is chosen.
+ * [OperatingSystemFamily] attribute in Gradle. It ensures the variant matching
+ * the current operating system is chosen (if there is such a candidate).
  *
- * The current operating system's attribute value must be set in the companion
- * object property [currentOsAttributeValue] during the project configuration
- * phase. This value is then used to determine the closest match among the
- * provided candidates during dependency resolution.
- *
- * @param currentOsAttributeValue the [OperatingSystemFamily] attribute value corresponding
+ * @param currentOsAttribute the `OperatingSystemFamily` attribute corresponding
  * to the current operating system.
- * @param objects the Gradle [ObjectFactory].
  */
 open class CurrentOsAttributeDisambiguationRule @Inject constructor(
 	private val currentOsAttribute: OperatingSystemFamily,
